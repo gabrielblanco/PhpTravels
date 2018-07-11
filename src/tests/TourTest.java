c5f9f91c85199f11a7f5c752a8e3102829c09437
AlonsoHerrera <alonsohf055@gmail.com>
2018-07-11 08:33:22 -0600

    Test case 07 works

--- src/tests/TourTest.java
+++ src/tests/TourTest.java
@@ -1,5 +1,7 @@
 package tests;
 
+import static org.testng.Assert.assertTrue;
+
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -8,12 +10,18 @@ import pageobjects.HeaderObjects;
 import pageobjects.ToursPage;
 
 public class TourTest extends BaseTest {
-		ToursPage toursPage;
-		HeaderObjects headeObjects;
+	 ToursPage toursPage;
+	 HeaderObjects headeObjects;
+		
 	  @BeforeClass
-	    public void setUp() {
+	  public void setUp() {
 		driver=getDriver();
-	    }
+	  }
+	  
+	  /**
+	   * Verify that the system displays an alert when user tries to add a tour to the wish list and isn’t  logged.
+	   * @throws InterruptedException
+	   */
 	  @Test
 	  public void alertIsDisplayed() throws InterruptedException {
 		  headeObjects= new HeaderObjects(driver);
@@ -21,6 +29,8 @@ public class TourTest extends BaseTest {
 		  Thread.sleep(2000);
 		  toursPage= new ToursPage(driver);
 		  toursPage.addTourToWishList();
+		  Thread.sleep(2000);
+		  assertTrue(toursPage.isAlertPresent(), "Alert isn't present!");
 	  }
 	  
 	  /**
