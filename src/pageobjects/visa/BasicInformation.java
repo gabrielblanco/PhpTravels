e75df84bd29ab779cf40f3f552a1f393d9f0490f
gapuser <gapuser@GAP-LPT-1723.gap.com>
2018-07-12 13:50:18 -0600

    Add imports to Visa view.

--- src/pageobjects/visa/BasicInformation.java
+++ src/pageobjects/visa/BasicInformation.java
@@ -5,7 +5,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.util.concurrent.TimeUnit;
 
 public class BasicInformation {
